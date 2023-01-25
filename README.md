f4e131ff655e5decb80f2ed0f7ed58cef89a3232
Родак Николай Игоревич <n.rodak@sdk.ru>
2023-01-25 16:04:52 +0300

    CI

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/xqhmof80sx2mxub8?svg=true)](https://ci.appveyor.com/project/Nirodak/bdd)
--- src/test/java/test/TestForm.java
+++ src/test/java/test/TestForm.java
@@ -1,4 +1,11 @@
 package test;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TestForm {
+    @BeforeEach
+    void setup() {open("http://localhost:9999")};
+//    void setup() {
+//        open("http://localhost:9999");
+//    }
 }
